45c8659b521ce1abb190b6634fa6fe5537d4d3e3
Augusto <augusto.vanderley@g.globo>
2022-02-01 15:38:02 -0300

    first commit

---
+++ test_maths.py
@@ -0,0 +1,7 @@
+import augustosimplelib.math_calculations as mymod
+
+def test_add_is_working():
+    assert mymod.myadd(2, 3) == (2 + 3)
+
+def test_sub_is_working():
+    assert mymod.mysub(2, 3) == (2 - 3)    
